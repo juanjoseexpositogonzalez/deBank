90375c16732b237ceb14c30f5c229dcdc6cb6864
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-01-08 20:19:06 +0100

    Add Etherscan API key configuration to Hardhat setup for enhanced contract verification. This allows for automatic verification of contracts on Etherscan using the provided API key.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+
 require("dotenv").config();
 const privateKeys = process.env.PRIVATE_KEYS || ""
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -10,5 +11,8 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: privateKeys.split(":")
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || ""
   }
 };
