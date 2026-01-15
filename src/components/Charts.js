bbf0a3037de4b2c869f9bcd89b83a4634ef1988c
Juanjo <expositogonzalez.juanjose@gmail.com>
2026-01-15 15:29:23 +0100

    Implement Charts component with Redux state management and loadStrategyReturns integration for fetching strategy returns. Enhance loading state handling with Loading component.

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,4 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { ethers } from 'ethers';
+
+import { useEffect } from 'react';
+
+import Loading from './Loading';
+
+import {
+    loadStrategyReturns,
+} from '../store/interactions';
+
+
 const Charts = () => {
+    const chainId = useSelector(state => state.provider.chainId);
+    
+    const provider = useSelector(state => state.provider.connection);
+    
+    const tokens = useSelector(state => state.tokens.contracts);
+    const symbols = useSelector(state => state.tokens.symbols);
+    
+    const strategyRouter = useSelector(state => state.strategyRouter.contract);
+    const dBankSymbol = useSelector(state => state.dBank.symbol);    
+    const shares = useSelector(state => state.dBank.shares);
+    const balances = useSelector(state => state.tokens.balances);
+    const dBank = useSelector(state => state.dBank.contract);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        loadStrategyReturns(provider, strategyRouter, dispatch)
+
+    }, [])
+
     return (
         <div>
             Charts
